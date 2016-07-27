4f7c619f965b62d9625849c2d52627d5fd661f59
Henk Bakker <spike1292@live.nl>
2016-07-27 22:54:06 +0200

    example listview with attribute styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,17 +5,30 @@ import {Component} from "@angular/core";
     templateUrl: "app.component.html",
 })
 export class AppComponent {
-    public counter: number = 16;
-
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
-    }
-    
-    public onTap() {
-        this.counter--;
-    }
+    public items:Array<{date:String, body:String, custom:boolean, type:string}> = [{
+        date:'2011-06-24',
+        body:'Hello',
+        custom:true,
+        type:'start'
+    }, {
+        date:'2011-06-25',
+        body:'Hello 2',
+        custom:false,
+        type:'start'
+    }, {
+        date:'2011-06-26',
+        body:'Hello 3',
+        custom:false,
+        type:'end'
+    },{
+        date:'2011-06-27',
+        body:'Hello 4',
+        custom:true,
+        type:'start'
+    },{
+        date:'2011-06-28',
+        body:'Hello 5',
+        custom:false,
+        type:'start'
+    }]
 }
